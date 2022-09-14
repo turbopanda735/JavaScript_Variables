8709daa6ede3538445c763ca6eaebfaf9bb4febf
Joel <franco4prez@gmail.com>
2022-09-14 16:21:17 -0400

    completed exercise

--- app.js
+++ app.js
@@ -4,7 +4,31 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Jol";
+let lastName = "K";
+var age = 24;
 
 // Exercise 2
+firstName = "Joel";
+lastName = "Ko"
+age = 25;
 
 // Exercise 3
+let language = "JavaScript"
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+//Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+//Exercise 5
+let title = "Name of the Wind"
+let author = "Patrick Rothfuss"
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
